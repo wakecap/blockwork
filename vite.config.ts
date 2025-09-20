dce999c42265100d660ffad17b0c2ca9156dfbbf
Hamed Farag <hamed@wakecap.com>
2025-09-20 09:44:23 +0300

    fix: replace react-scripts with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
   build: {
     target: 'esnext',
     minify: 'esbuild',
@@ -32,4 +38,4 @@ export default defineConfig({
       overlay: false,
     },
   },
-}); 
+}) 
